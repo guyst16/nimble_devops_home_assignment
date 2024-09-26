5bade4f083bd675704f9caaa4ca0c972249426d7
Guy Steinberger <100173467+guyst16@users.noreply.github.com>
2024-09-26 11:50:04 +0300

    Update main.go

--- Docker_resources/main.go
+++ Docker_resources/main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Nimble - Guy Steinberger !")
+	fmt.Fprintf(w, "Hello Nimble - Guy Steinberger !!")
 }
